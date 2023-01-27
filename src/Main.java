cfb11ac796c12bc95a4e41787db45939b13c34c6
Arman Varekyan <desmondfreight@gmail.com>
2023-01-28 00:00:30 +0400

    homework

---
+++ src/Main.java
@@ -0,0 +1,157 @@
+import javax.swing.plaf.TabbedPaneUI;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.util.Scanner;
+
+public class Main {
+// Primitive Data Types
+    public static void main(String[] args) {
+
+
+
+// Primitive Data Types
+
+
+       /*  byte a = 21;
+         byte b = 25;
+
+        System.out.printf(" Byte Max:%d \n",Math.max(a,b));
+        System.out.printf(" Byte Min:%d \n",Math.min(a,b));
+
+
+        short c = 35;
+        short d = 42;
+        System.out.printf(" Short Max:%d \n",Math.max(c,d));
+        System.out.printf("Short Min:%d \n",Math.min(c,d));
+
+
+
+         int e = 154;
+         int f = 267;
+        System.out.printf("Int Max:%d \n",Math.max(e,f));
+        System.out.printf("Int Min:%d \n",Math.min(e,f));
+
+
+
+
+         long g = 45687;
+         long h = 654879;
+        System.out.printf("Long Max:%d \n",Math.max(g,h));
+        System.out.printf("Long Min:%d \n",Math.min(g,h));*/
+
+
+//Group (Floating point number )
+     /*   float j = 799f;
+        float k = 1564f;
+        System.out.println(j+"\n"+k);
+
+        double l = 35479d;
+        double m = 5468451d;
+
+        System.out.println(l+"\n"+m);*/
+
+       /* float j = 799f;
+        float k = 1564f;
+        System.out.printf("Float Max:%f \n",Math.max(j,k));
+        System.out.printf("Float Min:%f \n",Math.min(j,k));
+
+        double l = 35479;
+        double m = 5468451;
+
+        System.out.printf("Double Max:%f \n ",Math.max(l,m));
+        System.out.printf("Double Min:%f \n",Math.min(l,m));*/
+
+        /*
+double a = 34.567839023d;
+float b = 12.345f;
+double c =  8923.1234257d;
+float d = 3456.091f;
+        System.out.println(a+"\n"+b+"\n"+c+"\n"+d);*/
+
+
+        //  Group (Character)
+
+/* char n = 65;
+char o = 78;
+        System.out.println(n+ "\n"+o);*/
+
+       /* char s = 77;
+        char w = 109;
+        System.out.println(s+"\n"+w);
+
+        char t = 57;
+        System.out.println(t);*/
+
+        // Group (Boolean)
+
+       /* boolean p = true;
+        boolean q = false;*/
+
+       /* int year = 201l;   // this is wrong command, because 'l' is symbol;
+        System.out.println(year);*/
+
+
+
+        //Java Type Casting
+
+       /* int aa = 7;
+        long bb;
+         bb = aa;
+        System.out.println(aa);
+        System.out.println(bb);*/
+
+        /*int myInt = 7;
+        double myDouble= myInt;
+
+        System.out.println(myInt);
+        System.out.println(myDouble);*/
+
+      /*  double dd = 9.78d ;
+        int ii = (int) dd ;
+
+        System.out.println(dd);
+        System.out.println(ii);*/
+
+/*int ff = 9999 ;
+byte gg = (byte) ff ;
+        System.out.println(ff);
+        System.out.println(gg);*/
+
+
+       // long max = 3123456789  // is large
+        /*long max = 312345678;*/
+
+
+//double d = 1000_.00; // '_', '.' is not correct commands
+  /*  double d = 100000;    */
+
+     //   char i = 'i'; char j = 'j'; char ij= i+j; // i & j is already assign,i+j is wrong command for type char;
+
+//int 7a = 8; // a is symbol is not working for type int ;
+
+
+       // long h = h+8; //'h' is already assign ;
+//long h = 8; // is right
+
+
+//int n,m = 11 ; // it`s wrong syntax
+//m = n;
+/*int n=11;
+int m=11;
+m=n;
+        System.out.println(n);
+        System.out.println(m);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
